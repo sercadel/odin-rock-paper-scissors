c03e6afedff896650f30b9994efe8b335e4f4f22
sercadel <37784445+sercadel@users.noreply.github.com>
2026-04-10 21:27:48 +0200

    Add getComputerChoice function with random selection logic

---
+++ script.js
@@ -0,0 +1,9 @@
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
+}
+
+// resultado en consola
+console.log(getComputerChoice())
+
